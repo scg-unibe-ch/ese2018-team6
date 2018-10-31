8520f6ea8ec9f1a548786d50ef382ef8458f9548
Roland Widmer <roland.widmer@students.unibe.ch>
2018-10-31 11:44:24 +0100

    extended jobitem model

--- ESE-2018-Scaffolding/backend/app/models/jobitem.model.ts
+++ ESE-2018-Scaffolding/backend/app/models/jobitem.model.ts
@@ -12,6 +12,33 @@ export class JobItem extends Model<JobItem> {
   description!: string;
 
   @Column
+  startDate!: Date;
+
+  @Column
+  endDate!: Date;
+
+  @Column
+  validUntil!: Date;
+
+  @Column
+  workloadMin!: number;
+
+  @Column
+  workloadMax!: number;
+
+  @Column
+  languages!: string;
+
+  @Column
+  location!: string;
+
+  @Column
+  salaryType!: number;
+
+  @Column
+  salaryAmount!: number;
+
+  @Column
   skills!: string;
 
   @Column
@@ -20,32 +47,41 @@ export class JobItem extends Model<JobItem> {
   @Column
   messageFromAdmin!: string;
 
-  /*@ForeignKey(() => JobList)
-  @Column
-  jobListId!: number;
-
-  @BelongsTo(() => JobList)
-  jobList!: JobList;
-  */
   toSimplification(): any {
     return {
       'id': this.id,
       'title': this.title,
       'description': this.description,
       'skills': this.skills,
+      'startDate': this.startDate,
+      'endDate': this.endDate,
+      'validUntil': this.validUntil,
+      'workloadMin': this.workloadMin,
+      'workloadMax': this.workloadMax,
+      'languages': this.languages,
+      'location': this.location,
+      'salaryType': this.salaryType,
+      'salaryAmount': this.salaryAmount,
       'accepted': this.accepted,
       'messageFromAdmin': this.messageFromAdmin,
-      // 'jobListId': this.jobListId
     };
   }
 
   fromSimplification(simplification: any): void {
     this.title = simplification['title'];
     this.description = simplification['description'];
-    // this.jobListId = simplification['jobListId'];
     this.skills = simplification['skills'];
+    this.startDate = simplification['startDate'];
+    this.endDate = simplification['endDate'];
+    this.validUntil = simplification['validUntil'];
+    this.workloadMin = simplification['workloadMin'];
+    this.workloadMax = simplification['workloadMax'];
+    this.languages = simplification['languages'];
+    this.location = simplification['location'];
+    this.salaryType = simplification['salaryType'];
+    this.salaryAmount = simplification['salaryAmount'];
     this.accepted = simplification['accepted'];
-    this.messageFromAdmin = simplification['messageFromAdmin'];
+    // this.messageFromAdmin = simplification['messageFromAdmin']; Companies must not change this column.
   }
 
 }
